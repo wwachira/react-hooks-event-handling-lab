24e71031ba5c300484b2836a4759a827980551ae
lizwachira <lizwanguwachira@gmail.com>
2024-05-12 20:08:16 +0300

    feat: console displays pwd entry

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+
+// Code EyesOnMe Component Here
+import React from "react";
+
+const EyesOnMe = () => {
+  const focus = () => {
+    console.log("Good!");
+  };
+
+  const blur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  return (
+    <button onFocus={focus} onBlur={blur}>
+      Eyes on me
+    </button>
+  );
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,15 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+const Keypad = () => {
+  const handleInputChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <input type="password" onChange={handleInputChange} />
+    </div>
+  );
 }
 
 export default Keypad;
